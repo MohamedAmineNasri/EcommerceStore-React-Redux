9eb33952ed3521dd9ec7167788e216c30f21bff7
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-12-17 18:20:30 +0100

    Add Redux reducers to manage application state

---
+++ src/containers/redux/reducers/productReducers.jsx
@@ -0,0 +1,18 @@
+import { ActionTypes } from "../constants/actionTypes";
+
+const initialState = {
+    products:[{
+        id:1,
+        title:'Nasri',
+        category:'programmer'
+    }]
+}
+
+export const productReducer = (state, {type, payload}) => {    
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state;
+    }
+}
