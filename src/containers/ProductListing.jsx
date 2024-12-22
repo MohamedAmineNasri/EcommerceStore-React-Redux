924ed0b8c156385902ca3d39642dc4c2b54f9512
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-12-22 21:13:27 +0100

    Implement useDispatch for action dispatching and update the Redux store

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,13 +1,27 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ProductComponent from "./ProductComponent";
+import { setProduts } from "../redux/actions/productActions";
+import axios from "axios";
 
 const ProductListing = () => {
-  const products= useSelector((state) => state.allProducts.products)
-  console.log(products)
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch()
+  const fetchProducts = async () => {
+    const respone = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("Err", err);
+      });
+      dispatch(setProduts(respone.data));
+  };
+  useEffect(()=>{
+    fetchProducts()
+  },[])
+  console.log("Products: ",products);
   return (
-    <div className="ui grid container" style={{marginTop: '30px'}}>
-     <ProductComponent />
+    <div className="ui grid container" style={{ marginTop: "30px" }}>
+      <ProductComponent />
     </div>
   );
 };
