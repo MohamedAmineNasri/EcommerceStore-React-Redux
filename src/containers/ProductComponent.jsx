596292ee158a97f32b303ea0d34ff91d52eb4930
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2025-01-01 16:59:04 +0100

    Render products listing page and product detail page; add select and remove action types

--- src/containers/ProductComponent.jsx
+++ src/containers/ProductComponent.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const ProductComponent = () => {
-  const products= useSelector((state) => state.allProducts.products)
-  // const{id, title} = products[0]
-  return (
-    <div className="four cloumn wide">
-      <div className="ui llink cards">
-        <div className="card">
-          <div className="image"></div>
+  const products = useSelector((state) => state.allProducts.products);
+
+  const renderList = products.map((product) => {
+    const { id, title, image, price, category } = product;
+
+    return (
+      <div className="four wide column" key={id} style={{ backgroundColor: "white" }}>
+        <Link to={`/product/${id}`}>
+        <div className="ui card" style={{ height: "350px" }}>
+          <div className="image" style={{ height: "200px", overflow: "hidden" }}>
+            <img
+              className="ui fluid image"
+              src={image}
+              alt={title}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
+          </div>
           <div className="content">
-            {/* <div className="header">{title}</div> */}
+            <div className="header">{title}</div>
+            <div className="meta price">${price}</div>
+            <div className="meta">{category}</div>
           </div>
         </div>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <>{renderList}</>;
 };
 
 export default ProductComponent;
--- src/redux/actions/productActions.jsx
+++ src/redux/actions/productActions.jsx
@@ -11,3 +11,9 @@ export const selectedProduct = (product) => {
         payload: product,
     }
 }
+
+export const removeSelectedProduct = () => {
+    return {
+        type:ActionTypes.REMOVE_SELECTED_PRODUCT,
+    }
+}
